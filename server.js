652b96b5f4b0f26853d8c6f414356eaebd019679
anshuman9999 <anshuman.pandey9999@gmail.com>
2021-02-23 18:56:35 +0530

    added create document post route

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express')
 const app = express()
-
 const mongoose = require('mongoose')
+const DocModel = require('./docModel')
 
 mongoose.connect('mongodb://localhost/pagination', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB connection successful!'))
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("Home Page")
-})
+app.post('/api/docs', createNewDocument)
+
+async function createNewDocument(req, res) {
+    try {
+        const newDoc = await DocModel.create(req.body)
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                doc: newDoc
+            }
+        })
+
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
+}
 
 app.listen(8000, () => {
     console.log("server started at port: 8000")
