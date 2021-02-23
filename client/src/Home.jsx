28c7602197f95c6f3d24d039e022139e20f12886
anshuman9999 <anshuman.pandey9999@gmail.com>
2021-02-23 19:10:17 +0530

    making post request when creating a doc

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Home = (props) => {
+    const [title, setTitle] = useState("")
+
     const clickHandler = () => {
+        async function createNewDoc() {
+            const newDoc = await axios.post('/api/docs', {
+                name: title
+            })
+
+            console.log(newDoc.data.data.doc)
+        }
+
+        createNewDoc()
+
         props.history.push({
             pathname: "/new"
         })
@@ -12,13 +25,27 @@ const Home = (props) => {
             <form>
 
                 <label>Title: </label>
-                <input type="text" placeholder="Enter title of the document" />
-                <button type="submit" onClick={
+                
+                <input 
+                    type="text" 
+                    placeholder="Enter title of the document" 
+                    required
+                    onChange={ (e) => {
+                        setTitle(e.target.value)
+                    } }
+                />
+
+                <button 
+                    type="submit" 
+                    onClick={
                     (e) => {
                         e.preventDefault()
                         clickHandler()
                     }
-                } >Create</button>
+                }
+                disabled={ !title } >
+                    Create
+                </button>
 
             </form>
         </>
