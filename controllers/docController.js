e585b6c418f4ee2194689884925f2fb74dd46f68
anshuman9999 <anshuman.pandey9999@gmail.com>
2021-03-04 13:17:18 +0530

    refactored server code into controllers

---
+++ controllers/docController.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose')
+const DocModel = require('../models/docModel')
+
+exports.getAllDocs = async function (req, res) {
+    try {
+        const docs = await DocModel.find({})
+
+        res.status(200).json({
+            status: 'success',
+            results: docs.length,
+            data: {
+                docs
+            }
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        })
+    }
+}
+
+exports.createNewDocument = async function (req, res) {
+    try {
+        const newDoc = await DocModel.create(req.body)
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                doc: newDoc
+            }
+        })
+
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        })
+    }
+}
+
+exports.getSingleDoc = async function (req, res) {
+    try {
+        const doc = await DocModel.findById(req.params.id)
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                doc
+            }
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
+}
+
+exports.updateDoc = async function (req, res) {
+    try {
+        const updatedDoc = await DocModel.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        })
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                doc: updatedDoc
+            }
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        })
+    }
+}
+
+exports.deleteDoc = async function (req, res) {
+    try {
+        await DocModel.findByIdAndDelete(req.params.id)
+
+        res.status(200).json({
+            status: 'success',
+            message: 'document deleted'
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        })
+    }
+}
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 const DocModel = require('./models/docModel')
 
+const { getAllDocs, createNewDocument, getSingleDoc, updateDoc, deleteDoc } = require('./controllers/docController')
+
 mongoose.connect('mongodb://localhost/pagination', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(() => console.log('DB connection successful!'))
 
@@ -10,105 +12,14 @@ app.use(express.json())
 
 app.get('/api/docs', getAllDocs)
 
-async function getAllDocs(req, res) {
-    try {
-        const docs = await DocModel.find({})
-
-        res.status(200).json({
-            status: 'success',
-            results: docs.length,
-            data: {
-                docs
-            }
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err.message
-        })
-    }
-}
-
 app.get('/api/docs/:id', getSingleDoc)
-
-async function getSingleDoc(req, res) {
-    try {
-        const doc = await DocModel.findById(req.params.id)
-
-        res.status(200).json({
-            status: 'success',
-            data: {
-                doc: doc
-            }
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        })
-    }
-}
 
 app.post('/api/docs', createNewDocument)
 
-async function createNewDocument(req, res) {
-    try {
-        const newDoc = await DocModel.create(req.body)
-
-        res.status(200).json({
-            status: 'success',
-            data: {
-                doc: newDoc
-            }
-        })
-
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        })
-    }
-}
-
 app.patch('/api/docs/:id', updateDoc)
-
-async function updateDoc(req, res) {
-    try {
-        const updatedDoc = await DocModel.findByIdAndUpdate(req.params.id, req.body, {
-            new: true
-        })
-
-        res.status(201).json({
-            status: 'success',
-            data: {
-                doc: updatedDoc
-            }
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err.message
-        })
-    }
-}
 
 app.delete('/api/docs/:id', deleteDoc)
 
-async function deleteDoc(req, res) {
-    try {
-        await DocModel.findByIdAndDelete(req.params.id)
-
-        res.status(200).json({
-            status: 'success',
-            message: 'document deleted'
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err.message
-        })
-    }
-}
 
 app.listen(8000, () => {
     console.log("server started at port: 8000")
